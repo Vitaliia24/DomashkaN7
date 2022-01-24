4087cc4856bfd3aac2d0a271c465e662f338748d
Vita <Vitayliyost@mail.ru>
2022-01-25 01:54:02 +0300

    выполнила дз номер 7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,26 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// задание 1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника " + fullName);
+
+        // задание 2
+        String s1 = fullName.toLowerCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + s1);
+
+        // задание 3
+        String s2 = fullName.replace(' ', ';');
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + s2);
+
+        // задание 4
+        String originalName = "Иванов Семён Семёнович";
+        String s3 = originalName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + s3);
+
+
     }
 }
